2d6cd3b37ebe8a9fc915d6cb96acc64e4aeafdbb
Kostya Rybak <62746770+K-Rybak@users.noreply.github.com>
2021-09-29 15:47:31 +0600

    update file

--- connect_to_bd.py
+++ connect_to_bd.py
@@ -3,8 +3,9 @@ from pymongo import MongoClient
 try:
     client = MongoClient("mongodb+srv://user_rybak_test:dad454221@cluster0.c4w91.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db = client.get_database('test_db')
-except:
+except Exception as e:
     print('Не удалось подключится к базе данных')
+    print(e)
 
 
 def get_empl_collection():
